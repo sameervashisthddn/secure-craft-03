4b74ec1fdc8cf10a784e2452c9e807331fa83ec8
sameervashisthddn <sameervashisth@gmail.com>
2026-02-19 01:00:32 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/',
   server: {
-    base: '/',
     host: "::",
     port: 8080,
     hmr: {
